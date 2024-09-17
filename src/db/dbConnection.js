814cb69ac6b56b47ca0261289e62763e2168e4c4
Vishal Dogra <dogravishal1598@gmail.com>
2024-09-17 15:03:12 +0530

    change in console message

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `\n MONGODB connected! DB Host: ${connectionInstance.connection.host}`
+      `\nMONGODB connected! DB Host: ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("MONGODB connection failed", error);
